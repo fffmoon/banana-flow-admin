0e1e15a79aa8e2d959d7fb0c5c886bce9ddb1832
fffmoon <xiangqing1997@outlook.com>
2025-10-26 15:19:27 +0800

    fix(upload): 上传报错
    -- 1. 尝试解决生产环境下上传报错问题

--- vite/plugins/mock.ts
+++ vite/plugins/mock.ts
@@ -2,7 +2,7 @@
  * @Author: Qing
  * @Description: mock数据
  * @Date: 2024-06-21 21:04:24
- * @LastEditTime: 2025-04-23 15:52:57
+ * @LastEditTime: 2025-10-26 15:14:20
  */
 import { viteMockServe } from 'vite-plugin-mock'
 
@@ -12,7 +12,7 @@ export default function setupMockPlugin(env: ImportMetaEnv) {
     ignore: /^_/,
     mockPath: 'mock', // 明确指定 mock 目录
     localEnabled: VITE_MOCK_SW,
-    prodEnabled: VITE_MOCK_SW,
+    prodEnabled: false, // 生产环境不开启 mock，遇到了生产环境下上传报错问题：https://blog.csdn.net/qq_45625778/article/details/151858411
     logger: true,
     injectCode: `
       import { setupProdMockServer } from '../mock/_createProductionServer';
